00d9477ddd37279a0dff448493e6df9cee887887
solitdio079 <solitdio079@gmail.com>
2025-03-30 23:36:47 +0300

    Add some javascript exercises

---
+++ js_foundations/script.js
@@ -0,0 +1,8 @@
+const jsOfficialName = prompt("What is the official name of JavaScript: ")
+
+if (jsOfficialName === "ECMAScript") {
+    alert("Right")
+} else {
+    alert("You don't know? ECMAScript!")
+}
+
