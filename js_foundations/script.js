1ece8039b3d661ec3120e6d1bf54c05b0732dd1f
solitdio079 <solitdio079@gmail.com>
2025-04-06 05:16:38 +0300

    Add functions basics practice

--- js_foundations/script.js
+++ js_foundations/script.js
@@ -1,8 +1,18 @@
-const jsOfficialName = prompt("What is the official name of JavaScript: ")
 
-if (jsOfficialName === "ECMAScript") {
-    alert("Right")
-} else {
-    alert("You don't know? ECMAScript!")
-}
+    const x = 1;
 
+    function a() {
+      const y = 2;
+      output(z)
+    }
+
+    function b() {
+      const z = 3;
+      output(y)
+    }
+
+    function output(value) {
+      const para = document.createElement('p');
+      document.body.appendChild(para);
+      para.textContent = `Value: ${value}`;
+    }
